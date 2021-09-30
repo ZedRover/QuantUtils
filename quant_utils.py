7316d382f12f28068ffbf30f9e684161b2ed570b
yu.wang <yu.wang@brightridge.net>
2021-09-30 14:40:09 +0800

    .

--- quant_utils.py
+++ quant_utils.py
@@ -9,7 +9,7 @@ from qrutils.future_utils import get_X_and_snapshot_baptized, get_all_predictors
 from qrutils.future_utils import get_product_information,get_rolling_product,get_X_and_snapshot
 pd.set_option('display.max_columns', None)
 import datetime
-
+from sfutils import signal_utils as sgu 
 
 def get_model_forecaster(prod='i0001',
                          bgn_date=20210901,
@@ -147,5 +147,43 @@ def plot_y_for_x(x,y,thres,direction = 'x-y'):
 
 
 
+
+
+def _calc_skew(i, trading_days, rolling_window, log_r, signal):
+    logr_i = log_r.loc[str(trading_days[i]):str(trading_days[i + rolling_window-1])].reset_index(drop=1).values.flatten()
+    signals_i = signal.loc[str(trading_days[i]):str(trading_days[i + rolling_window-1])].reset_index(drop=1).values.flatten()
+    thres = get_oc_threshold('i1')
+    # corr_num = signals_i.corrwith(logr_i)
+    skew_num = []
+    skew_num.append(stats.skew(sgu.xy_finder(signals_i,logr_i,thres,reverse = True)))
+    skew_num.append(stats.skew(sgu.xy_finder(signals_i,logr_i,-thres,reverse = True)))
+    
+    
+    return str(trading_days[i + rolling_window-1]), skew_num 
+
+
+
+def calc_rolling_skew(time,log_r,signals,rolling_window):
+    if len(signals.shape)==2:
+        columns = list(range(signals.shape[1]))
+    else:
+        columns = [0]
+    if hasattr(log_r, 'values'):
+        log_r = log_r.values
+    if hasattr(signals, 'columns'):
+        columns = signals.columns
+    if hasattr(signals, 'values'):
+        signals = signals.values
+    log_r = pd.Series(log_r, index=pd.to_datetime(time))
+    signals = pd.DataFrame(signals, index=pd.to_datetime(time))
+    trading_days = pd.unique(log_r.index.date)
+    result_dic = {}
+    for i in list(range(len(trading_days) - rolling_window +1)):
+        k = _calc_skew(i, trading_days, rolling_window, log_r, signals)
+        result_dic[k[0]] = k[1]
+        
+    rolling_skew = pd.DataFrame(result_dic).T
+    rolling_skew.columns = ['taking +','taking -']
+    return rolling_skew 
 
 
